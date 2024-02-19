8abc0b1fd3a799dee0999dacc2b9283071a2b80f
preeti tambe <preetitambe158@gmail.comgit config --global user.name priti>
2024-02-19 16:51:30 +0530

    Created servlet page

--- src/EmpDao.java
+++ src/EmpDao.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class EmpDao {
@@ -16,6 +17,25 @@ public class EmpDao {
 			e.printStackTrace();
 		}
 		return con;
+		
+	}
+	
+	public static int save(Emp e)
+	{
+		int status=0;
+		
+		try {
+			Connection con= EmpDao.getConnection();
+			PreparedStatement ps= con.prepareStatement("insert into user15 (name,password,email,country)values(?,?,?,?)");
+			
+			ps.setString(1, e.getName());
+			
+			status=ps.executeUpdate();
+		} catch (SQLException e1) {
+			
+			e1.printStackTrace();
+		}
+		return status;
 		
 	}
 
---
+++ src/SaveServlet.java
@@ -0,0 +1,62 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SaveServlet
+ */
+@WebServlet("/SaveServlet")
+public class SaveServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SaveServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		String name=request.getParameter("name");
+		
+		Emp e=new Emp();
+		e.setName(name);
+		
+		int status=EmpDao.save(e);
+		if(status>0)
+		{
+			out.print("Record save Successfully");
+			request.getRequestDispatcher("index.html").include(request, response);
+		}
+		else
+		{
+			out.print("Sorry !! Unable to save record");
+		}
+		out.close();
+	}
+
+}
